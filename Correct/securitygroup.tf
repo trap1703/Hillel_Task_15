115c58664b0f8a9700442c169f803ebf3e4042ea
Sergey Savenkov <36662869+trap1703@users.noreply.github.com>
2020-02-29 17:57:18 +0200

    Add files via upload

---
+++ Correct/securitygroup.tf
@@ -0,0 +1,70 @@
+resource "aws_security_group" "SGloadBalance" {
+  name        = "SGloadBalance"
+  description = "Allow inbound traffic"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "SGloadBalance"
+  }
+}
+resource "aws_security_group" "SGEC2" {
+  name        = "SGEC2"
+  description = "Allow inbound traffic"
+
+  #  ingress {
+  #    from_port   = 80
+  #    to_port     = 80
+  #    protocol    = "tcp"
+  #    cidr_blocks = ["0.0.0.0/0"]
+  #  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.SGloadBalance.id]
+  }
+  egress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.SGloadBalance.id]
+  }
+  #  egress {
+  #    from_port   = 0
+  #    to_port     = 0
+  #    protocol    = "-1"
+  #    cidr_blocks = ["0.0.0.0/0"]
+  #  }
+  tags = {
+    Name = "SGloadBalance"
+  }
+}
